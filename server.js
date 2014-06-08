8dc70b07f68b58364babbf724fd38f170c05a1a8
Jose Contreras <jocontre@joselcontreras.com>
2014-06-07 20:24:11 -0700

    Error

--- server.js
+++ server.js
@@ -176,7 +176,7 @@ app.post('/api/shows', function(req, res, next) {
 
 // End routes
 
-//Er
+//Err
 app.set('port', process.env.PORT || 3000);
 app.use(logger('dev'));
 app.use(bodyParser.json());
